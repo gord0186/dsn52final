162e1693c201b852eea62f960e359581da08bddd
gord0186 <gord0186@algonqiuinlive.com>
2014-01-13 10:31:55 -0500

    added all the files from my project

---
+++ js/pop-down.js
@@ -0,0 +1,138 @@
+// JavaScript Document
+
+
+	var popDown = document.querySelector('.pop-down');
+	
+	document.querySelector('.pop-btn').addEventListener('click', function(e){
+		e.preventDefault(); 
+	
+	
+		if (popDown.getAttribute('data-state') == 'expanded'){
+			
+			popDown.setAttribute('data-state', 'collapsed');
+		
+		}else{
+		
+			popDown.setAttribute('data-state', 'expanded');
+		
+		}
+	});
+	
+	var popDown2 = document.querySelector('.pop-down2');
+	
+	document.querySelector('.pop-btn2').addEventListener('click', function(e){
+		e.preventDefault(); 
+	
+	
+		if (popDown2.getAttribute('data-state') == 'expanded'){
+			
+			popDown2.setAttribute('data-state', 'collapsed');
+		
+		}else{
+		
+			popDown2.setAttribute('data-state', 'expanded');
+		
+		}
+	});
+	
+	var popDown3 = document.querySelector('.pop-down3');
+	
+	document.querySelector('.pop-btn3').addEventListener('click', function(e){
+		e.preventDefault(); 
+	
+	
+		if (popDown3.getAttribute('data-state') == 'expanded'){
+			
+			popDown3.setAttribute('data-state', 'collapsed');
+		
+		}else{
+		
+			popDown3.setAttribute('data-state', 'expanded');
+		
+		}
+	});
+	
+	var popDown4 = document.querySelector('.pop-down4');
+	
+	document.querySelector('.pop-btn4').addEventListener('click', function(e){
+		e.preventDefault(); 
+	
+	
+		if (popDown4.getAttribute('data-state') == 'expanded'){
+			
+			popDown4.setAttribute('data-state', 'collapsed');
+		
+		}else{
+		
+			popDown4.setAttribute('data-state', 'expanded');
+		
+		}
+	});
+	
+	var popDown5 = document.querySelector('.pop-down5');
+	
+	document.querySelector('.pop-btn5').addEventListener('click', function(e){
+		e.preventDefault(); 
+	
+	
+		if (popDown5.getAttribute('data-state') == 'expanded'){
+			
+			popDown5.setAttribute('data-state', 'collapsed');
+		
+		}else{
+		
+			popDown5.setAttribute('data-state', 'expanded');
+		
+		}
+	});
+	
+	var popDown6 = document.querySelector('.pop-down6');
+	
+	document.querySelector('.pop-btn6').addEventListener('click', function(e){
+		e.preventDefault(); 
+	
+	
+		if (popDown6.getAttribute('data-state') == 'expanded'){
+			
+			popDown6.setAttribute('data-state', 'collapsed');
+		
+		}else{
+		
+			popDown6.setAttribute('data-state', 'expanded');
+		
+		}
+	});
+	
+	var popDown7 = document.querySelector('.pop-down7');
+	
+	document.querySelector('.pop-btn7').addEventListener('click', function(e){
+		e.preventDefault(); 
+	
+	
+		if (popDown7.getAttribute('data-state') == 'expanded'){
+			
+			popDown7.setAttribute('data-state', 'collapsed');
+		
+		}else{
+		
+			popDown7.setAttribute('data-state', 'expanded');
+		
+		}
+	});
+	
+	var popDown8 = document.querySelector('.pop-down8');
+	
+	document.querySelector('.pop-btn8').addEventListener('click', function(e){
+		e.preventDefault(); 
+	
+	
+		if (popDown8.getAttribute('data-state') == 'expanded'){
+			
+			popDown8.setAttribute('data-state', 'collapsed');
+		
+		}else{
+		
+			popDown8.setAttribute('data-state', 'expanded');
+		
+		}
+	});
